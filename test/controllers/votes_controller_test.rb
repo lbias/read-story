5a71b163583306f599580618c4d34c969258f3d2
lbias <306508354@qq.com>
2017-08-16 00:47:07 +0800

    test voting histroy order

--- test/controllers/votes_controller_test.rb
+++ test/controllers/votes_controller_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class VotesControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get votes_create_url
-    assert_response :success
-  end
+  # test "should get create" do
+  #   get votes_create_url
+  #   assert_response :success
+  # end
 
 end
