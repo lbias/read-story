92618df2a5f5b444dff34349811c27439caeadda
lbias <306508354@qq.com>
2017-08-16 00:52:59 +0800

    test regular http voting

--- test/controllers/votes_controller_test.rb
+++ test/controllers/votes_controller_test.rb
@@ -10,5 +10,10 @@ class VotesControllerTest < ActionDispatch::IntegrationTest
   test "create vote with ajax" do
     post story_votes_path(stories(:two)), xhr: true
     assert_response :success
-  end  
+  end
+
+  test "redirect after vote with http post" do
+    post story_votes_path(stories(:two))
+    assert_redirected_to story_path(stories(:two))
+  end    
 end
