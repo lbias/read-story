33ba06dc4a0f08a51b28e5412ce458d62bc43163
lbias <306508354@qq.com>
2017-08-17 19:18:10 +0800

    add votes_count to stories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170817085433) do
+ActiveRecord::Schema.define(version: 20170817111506) do
 
   create_table "stories", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170817085433) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "votes_count"
   end
 
   create_table "users", force: :cascade do |t|
