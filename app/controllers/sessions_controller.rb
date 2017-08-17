280b93a91cee5fe0de158ec7e808e22c5ef64ba1
lbias <306508354@qq.com>
2017-08-17 15:47:16 +0800

    controller sessions#create, view sessions#new

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @current_user = User.find_by(email: params[:email])
+    if @current_user && @current_user.authenticate(params[:password])
+      session[:user_id] = @current_user.id
+      redirect_to stories_path
+    else
+      render 'new'
+    end
   end
 
   def destroy
