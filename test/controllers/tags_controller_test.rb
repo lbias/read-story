7c91be9d9bec788c33cc1fbcc56a8d5fcfa13cb1
lbias <306508354@qq.com>
2017-08-18 11:38:29 +0800

    test the show action of controller tags

--- test/controllers/tags_controller_test.rb
+++ test/controllers/tags_controller_test.rb
@@ -1,9 +1,11 @@
 require 'test_helper'
 
 class TagsControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get tags_show_url
+  test "renders tagged stories" do
+    stories(:one).tag_list = 'blog, ruby'
+    stories(:one).save
+    get tag_path("ruby")
     assert_response :success
+    assert_select 'div#content div.story', count: 1
   end
-
 end
